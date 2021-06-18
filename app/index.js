4ca90523b57d590af0e9954cfd5de97e8baa3762
Shuvro77 <shuvrodutta91@gmail.com>
2021-06-18 13:51:58 +0600

    Applied React Router

--- app/index.js
+++ app/index.js
@@ -1,25 +1,66 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import './index.css'
+import Nav from './components/Nav'
+
+import { ThemeProvider } from './contexts/theme'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
 
 
 import Popular from './components/popular'
 import Battle from './components/battle'
+import Results from './components/result'
 
 class App extends React.Component {
-    render() {
-        return (
-            <div className = 'container'>
-                <Battle />
-            </div>
-            
-        )
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      theme: 'light',
+      toggleTheme: () => {
+        this.setState(({ theme }) => ({
+          theme: theme === 'light' ? 'dark' : 'light'
+        }))
+      }
     }
+  }
+
+
+
+
+  render() {
+    return (
+      <Router>
+        <ThemeProvider value={this.state}>
+          <div className={this.state.theme}>
+            <div className='container'>
+              <Nav />
+
+              <Switch>
+                <Route exact path='/' component={Popular} />
+                <Route exact path='/battle' component={Battle} />
+                <Route path='/battle/results' component={Results} />
+                <Route render={() => <h1>404</h1>} />
+              </Switch>
+            </div>
+          </div>
+
+        </ThemeProvider>
+
+
+      </Router>
+
+
+
+
+    )
+  }
 }
 
 ReactDOM.render(
-    <App />,
-    document.getElementById('app')
+  <App />,
+  document.getElementById('app')
 )
 
 export default App
